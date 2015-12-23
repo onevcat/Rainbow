d2967f11556c2937daa1f2c2006fce6ab14c709f
onevcat <onevcat@gmail.com>
2015-12-23 22:55:44 +0900

    Fix tests for rainbow

--- Tests/RainbowTests.swift
+++ Tests/RainbowTests.swift
@@ -14,7 +14,6 @@ class RainbowTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        Rainbow.outputTarget = .Console
     }
     
     override func tearDown() {
@@ -34,6 +33,12 @@ class RainbowTests: XCTestCase {
         XCTAssertNil(result2.backgroundColor)
         XCTAssertNil(result2.styles)
         XCTAssertEqual(result2.text, "\u{001B}[0mHello\u{001B}")
+        
+        let result3 = Rainbow.extractModesForString("\u{001B}[fg0,0,0;Hello\u{001B}")
+        XCTAssertNil(result3.color)
+        XCTAssertNil(result3.backgroundColor)
+        XCTAssertNil(result3.styles)
+        XCTAssertEqual(result3.text, "\u{001B}[fg0,0,0;Hello\u{001B}")
     }
     
     func testExtractModes() {
@@ -60,9 +65,24 @@ class RainbowTests: XCTestCase {
         XCTAssertNil(result4.backgroundColor)
         XCTAssertNil(result4.styles)
         XCTAssertEqual(result4.text, "\u{001B}[4;31;93mHello World\u{001B}[0m")
+        
+        let result5 = Rainbow.extractModesForString("\u{001B}[fg0,0,0;Hello\u{001B}[;")
+        XCTAssertEqual(result5.color!, Color.Black)
+        XCTAssertNil(result5.backgroundColor)
+        XCTAssertNil(result5.styles)
+        XCTAssertEqual(result5.text, "Hello")
+        
+        let result6 = Rainbow.extractModesForString("\u{001B}[fg0,0,0;\u{001B}[bg255,0,0;Hello\u{001B}[;")
+        XCTAssertEqual(result6.color!, Color.Black)
+        XCTAssertEqual(result6.backgroundColor, BackgroundColor.Red)
+        XCTAssertNil(result6.styles)
+        XCTAssertEqual(result6.text, "Hello")
     }
     
-    func testGenerateStringWithCodes() {
+    func testGenerateConsoleStringWithCodes() {
+        
+        Rainbow.outputTarget = .Console
+        
         let result1 = Rainbow.generateStringForColor(nil, backgroundColor: nil, styles: nil, text: "Hello")
         XCTAssertEqual(result1, "Hello")
         
@@ -71,5 +91,32 @@ class RainbowTests: XCTestCase {
         
         let result3 = Rainbow.generateStringForColor(.LightYellow, backgroundColor: .Magenta, styles: [.Bold, .Blink], text: "Hello")
         XCTAssertEqual(result3, "\u{001B}[93;45;1;5mHello\u{001B}[0m")
+    }
+    
+    func testGenerateXcodeColorsStringWithCodes() {
+        
+        Rainbow.outputTarget = .XcodeColors
+        
+        let result1 = Rainbow.generateStringForColor(nil, backgroundColor: nil, styles: nil, text: "Hello")
+        XCTAssertEqual(result1, "Hello")
+        
+        let result2 = Rainbow.generateStringForColor(.Red, backgroundColor: nil, styles: nil, text: "Hello")
+        XCTAssertEqual(result2, "\u{001B}[fg255,0,0;Hello\u{001B}[;")
+        
+        let result3 = Rainbow.generateStringForColor(.LightYellow, backgroundColor: .Magenta, styles: [.Bold, .Blink], text: "Hello")
+        XCTAssertEqual(result3, "\u{001B}[fg255,255,102;\u{001B}[bg255,0,255;Hello\u{001B}[;")
+    }
+    
+    func testGenerateUnknownStringWithCodes() {
+        Rainbow.outputTarget = .Unknown
+        
+        let result1 = Rainbow.generateStringForColor(nil, backgroundColor: nil, styles: nil, text: "Hello")
+        XCTAssertEqual(result1, "Hello")
+        
+        let result2 = Rainbow.generateStringForColor(.Red, backgroundColor: nil, styles: nil, text: "Hello")
+        XCTAssertEqual(result2, "Hello")
+        
+        let result3 = Rainbow.generateStringForColor(.LightYellow, backgroundColor: .Magenta, styles: [.Bold, .Blink], text: "Hello")
+        XCTAssertEqual(result3, "Hello")
     }
 }
