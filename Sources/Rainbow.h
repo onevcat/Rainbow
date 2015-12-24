7513fc8a133b78ce6f13ec574afdc4f4ab66911b
onevcat <onevcat@gmail.com>
2015-12-24 14:53:14 +0900

    Add targets for all platforms

--- Sources/Rainbow.h
+++ Sources/Rainbow.h
@@ -24,7 +24,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Rainbow.
 FOUNDATION_EXPORT double RainbowVersionNumber;
