c63d7104ba9f3f4d3d9032057b421eaf5f7be282
Vincent Esche <regexident@gmail.com>
2017-11-27 10:06:01 +0100

    Improved Swift Manifest file

--- Package.swift
+++ Package.swift
@@ -1,10 +1,15 @@
 // swift-tools-version:4.0
 
 import PackageDescription
+
 let package = Package(
     name: "Rainbow",
+    products: [
+        .library(name: "Rainbow", targets: ["Rainbow"]),
+    ],
+    dependencies: [],
     targets: [
-        .target(name: "Rainbow", path: "Sources"),
-        .testTarget(name: "RainbowTests", dependencies: ["Rainbow"], path: "Tests")
+        .target(name: "Rainbow", dependencies: [], path: "Sources"),
+        .testTarget(name: "RainbowTests", dependencies: ["Rainbow"], path: "Tests"),
     ]
 )
